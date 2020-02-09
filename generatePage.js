b43262efb70cf46d91b0bf676017bf42b2e7f8de
Jeanette Yue <yue.jeanette@gmail.com>
2020-02-09 12:26:43 -0800

    added comments on event listeners

--- generatePage.js
+++ generatePage.js
@@ -42,12 +42,14 @@ document.onreadystatechange = () => {
     /**************************************EVENT LISTENERS*******************************************/
 
     // PRIMARY NAV------------------------------------------------------------------------------------
+    // slide primary nav out of view when user closes it
     document.querySelector('.primary-close').addEventListener('click', () => {
       let nav = document.querySelector('.primary-nav');
       nav.style.animation = 'slideOutNav 1s';
       setTimeout(() => nav.classList.add('inactive-nav'), 1000);
     });
 
+    // add event listener for each link in primary nav to slide popup into view
     document.querySelectorAll('.link').forEach(link => {
       link.addEventListener('click', () => {
         viewPopup(link.innerText.toLowerCase());
@@ -55,6 +57,7 @@ document.onreadystatechange = () => {
       });
     });
 
+    //slide primary nav into view when user clicks menu image
     document.querySelector('.menu').addEventListener('click', () => {
       document.querySelector('.primary-nav').style.animation = 'slideInNav 1s';
       document.querySelector('.inactive-nav').classList.remove('inactive-nav');
@@ -63,35 +66,39 @@ document.onreadystatechange = () => {
 
 
     // WIDGETS------------------------------------------------------------------------------------
-
+    // show selected widget from main screen
     document.querySelectorAll('.widgets > section').forEach(widget => {
       widget.addEventListener('click', () => viewPopup(widget.className.split('-')[0] + '-popup'));
     });
 
 
     // POPUPS + ACTIONS--------------------------------------------------------------------------
+    // makes close button in each popup and action section work
     document.querySelectorAll('.close').forEach(button => {
       button.addEventListener('click', () => closePopup(document.querySelector('.active-popup')));
     });
 
+    // show selected finance tab
     document.querySelectorAll('.finance-nav > li').forEach(tab => {
       let className = tab.classList[0];
       tab.addEventListener('click', () => viewFinanceTab(className, className + '-info'));
     });
 
+    // make crypto the default active finance tab
     document.querySelector('.crypto').classList.add('active-finance-tab');
     document.querySelector('.crypto-info').classList.add('active-finance');
 
-
+    // show news tab
     document.querySelectorAll('.news-nav > li').forEach(tab => {
       let className = tab.classList[0];
       tab.addEventListener('click', () => viewNewsTab(className, className + '-headlines'));
     });
 
+    // make world headlines the default active news tab
     document.querySelector('.world-headlines').classList.add('active-headlines');
     document.querySelector('.world').classList.add('active-news-tab');
 
-    
+
     document.querySelector('.location-btn').addEventListener('click', () => {
       if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(async position => {

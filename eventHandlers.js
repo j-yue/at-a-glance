e2b0033823efe344452683ebe194464a5d07a951
Jeanette Yue <yue.jeanette@gmail.com>
2020-02-08 19:46:33 -0800

    added comments

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,11 +1,7 @@
 
-// export function widgetClicked(sel) {
-// export function viewPopup(sel) {
-//     let className = sel.split('-')[0];
-//     let popup = document.querySelector(`.${className}-popup`);
-//     popup.classList.toggle('active-popup');
-// }
-
+  /**
+   * View popup screen selected from clicked widget in main screen
+   */
 export function viewPopup(sel) {
     let popup = document.querySelector(`.${sel}`);
     popup.classList.toggle('active-popup');
@@ -13,20 +9,43 @@ export function viewPopup(sel) {
 }
 
 
+/**
+   * Close active popup
+   */
 export function closePopup(sel) {
     sel.classList.toggle('active-popup');
 }
 
 
+  /**
+   * Remove a classname from element containing it
+   * used to toggle between active elements/screens
+   * @param  {string} name
+   */
 function removeClassName(name) {
     document.querySelector(`.${name}`).classList.remove(name);
 }
 
 
+  /**
+   * Add classname to element
+   * used to toggle between active elements/screens
+   * @param  {string} name
+   * @param {string} sel
+   */
 function addClassName(name, sel) {
     document.querySelector(`.${sel}`).classList.add(name);
 }
 
+
+  /**
+   * Switch active tabs
+   * used in news and finance screens
+   * @param  {string} activeTab
+   * @param  {string} activeContent
+   * @param  {string} selectedTab
+   * @param  {string} selectedContent
+   */
 function viewTab(activeTab, activeContent, selectedTab, selectedContent) {
     removeClassName(activeTab);
     removeClassName(activeContent);
@@ -34,12 +53,16 @@ function viewTab(activeTab, activeContent, selectedTab, selectedContent) {
     addClassName(activeContent, selectedContent);
 }
 
-
+/**
+   * View selected news tab
+   */
 export function viewNewsTab(tab, headlines) {
     viewTab('active-news-tab', 'active-headlines', tab, headlines);
 }
 
-
+/**
+   * View selected finance tab
+   */
 export function viewFinanceTab(tab, content) {
     viewTab('active-finance-tab', 'active-finance', tab, content);
 }
